a7c83f15bb3b56fb8ab58e67c683c175606858bd
RetaxMaster <retaxmaster@hotmail.com>
2021-01-22 01:59:31 -0600

    /bin/bash

--- tests/parser_test.py
+++ tests/parser_test.py
@@ -40,3 +40,19 @@ class ParserTest(TestCase):
 
             self.assertEqual(statement.token_literal(), "variable")
             self.assertIsInstance(statement, LetStatement)
+
+
+    def test_names_in_let_statements(self) -> None:
+
+        source: str = """
+            variable x = 5;
+            variable y = 10;
+            variable foo = 20;
+        """
+
+        lexer: Lexer = Lexer(source)
+        parser: Parser = Parser(lexer)
+
+        program: Program = parser.parse_program()
+
+        print(program.statements[0].token_literal())
