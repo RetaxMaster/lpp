b995a9c5f85a1da1bb7f02c3e82aef8588e89970
RetaxMaster <retaxmaster@hotmail.com>
2021-01-24 00:43:53 -0600

    Actualizacion del REPL para agregar el parser

--- lpp/repl.py
+++ lpp/repl.py
@@ -1,4 +1,8 @@
+from typing import List
+
+from lpp.ast import Program
 from lpp.lexer import Lexer
+from lpp.parser import Parser
 from lpp.token import (
     Token,
     TokenType
@@ -7,13 +11,24 @@ from lpp.token import (
 EOF_TOKEN: Token = Token(TokenType.EOF, "")
 
 
+def _print_parse_errors(errors: List[str]):
+
+    for error in errors:
+        print(error)
+
+
 def start_repl():
     
     # Walrus operator, asigna a la vez que compara
     while (source := input(">> ")) != "salir()":
         
         lexer: Lexer = Lexer(source)
+        parser: Parser = Parser(lexer)
 
-        while (token := lexer.next_token()) != EOF_TOKEN:
-            
-            print(token)
+        program: Program = parser.parse_program()
+
+        if len(parser.errors) > 0:
+            _print_parse_errors(parser.errors)
+            continue
+
+        print(program)
