b2a2a79abcd2b662d2aa4bb272e47b02ea76ca25
RetaxMaster <retaxmaster@hotmail.com>
2021-01-24 04:02:54 -0600

    Adicion de los objetos, los tests del evaluador y la implementacion del evaluador

---
+++ lpp/evaluator.py
@@ -0,0 +1,53 @@
+from typing import (
+    cast,
+    List,
+    Optional
+)
+
+import lpp.ast as ast
+
+from lpp.object import (
+    Integer,
+    Object
+)
+
+
+def evaluate(node: ast.ASTNode) -> Optional[Object]:
+
+    node_type = type(node)
+
+    if node_type == ast.Program:
+
+        node = cast(ast.Program, node)
+
+        return _evaluate_statements(node.statements)
+
+    elif node_type == ast.ExpressionStatement:
+
+        node = cast(ast.ExpressionStatement, node)
+        
+        assert node.expression is not None
+
+        return evaluate(node.expression)
+
+    elif node_type == ast.Integer:
+        
+        node = cast(ast.Integer, node)
+
+        assert node.value is not None
+
+        # Este Integer NO es el mismo que el ast.Integer, este Integer es la representación en objeto de el nodo ast.Integer que estemos evaluando
+        return Integer(node.value)
+
+    return None
+
+
+def _evaluate_statements(statements: List[ast.Statement]) -> Optional[Object]:
+
+    result: Optional[Object] = None
+
+    for statement in statements:
+
+        result = evaluate(statement)
+
+    return result
