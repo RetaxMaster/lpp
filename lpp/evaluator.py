6caad960dc91ce87d3326d69aaa61eb8e2b2ba94
RetaxMaster <retaxmaster@hotmail.com>
2021-01-24 04:50:29 -0600

    Adicion de la evaluacion de los prefijos (bang y minus operators)

--- lpp/evaluator.py
+++ lpp/evaluator.py
@@ -54,6 +54,18 @@ def evaluate(node: ast.ASTNode) -> Optional[Object]:
 
         return _to_boolean_object(node.value)
 
+    elif node_type == ast.Prefix:
+
+        node = cast(ast.Prefix, node)
+        
+        # right es la expresión que está al lado del signo: 5, verdadero, falso... y nuestro evaluador ya saber como evaluar estas funciones así que nos retornará dichos objetos :D
+        assert node.right is not None
+        right = evaluate(node.right)
+
+        assert right is not None
+
+        return _evaluate_prefix_expression(node.operator, right)
+
     return None
 
 
@@ -66,6 +78,42 @@ def _evaluate_statements(statements: List[ast.Statement]) -> Optional[Object]:
         result = evaluate(statement)
 
     return result
+
+def _evaluate_bang_operator_expression(right: Object) -> Object:
+
+    if right is TRUE:
+        return FALSE
+
+    elif right is FALSE:
+        return TRUE
+    
+    elif right is NULL:
+        return TRUE
+
+    else:
+        return FALSE
+
+    
+def _evaluate_minus_operator_expression(right: Object) -> Object:
+
+    if type(right) != Integer:
+        return NULL
+
+    right = cast(Integer, right)
+
+    return Integer(-right.value)
+
+
+def _evaluate_prefix_expression(operator: str, right: Object) -> Object:
+
+    if operator == "!":
+        return _evaluate_bang_operator_expression(right)
+
+    elif operator == "-":
+        return _evaluate_minus_operator_expression(right)
+
+    else:
+        return NULL
 
 
 def _to_boolean_object(value: bool) -> Boolean:
