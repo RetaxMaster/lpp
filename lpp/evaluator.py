4172647b2bde9e5df96bcb17bd7111850a41cecb
RetaxMaster <retaxmaster@hotmail.com>
2021-01-25 01:04:49 -0600

    Adicion de la evaluacion de builtins

--- lpp/evaluator.py
+++ lpp/evaluator.py
@@ -7,9 +7,11 @@ from typing import (
 )
 
 import lpp.ast as ast
+from lpp.builtins import BUILTINS
 
 from lpp.object import (
     Boolean,
+    Builtin,
     Environment,
     Error,
     Function,
@@ -167,16 +169,23 @@ def evaluate(node: ast.ASTNode, env: Environment) -> Optional[Object]:
 
 def _apply_function(fn: Object, args: List[Object]) -> Object:
 
-    if type(fn) != Function:
+    if type(fn) == Function:
+        
+        fn = cast(Function, fn)
+
+        extended_environment = _extend_function_environment(fn, args)
+        evaluated = evaluate(fn.body, extended_environment)
+        
+        assert evaluated is not None
+        return _unwrap_return_value(evaluated)
+
+    elif type(fn) == Builtin:
+
+        fn = cast(Builtin, fn)
+        return fn.fn(*args)
+
+    else:
         return _new_error(_NOT_A_FUNCTION, [fn.type().name])
-
-    fn = cast(Function, fn)
-
-    extended_environment = _extend_function_environment(fn, args)
-    evaluated = evaluate(fn.body, extended_environment)
-    
-    assert evaluated is not None
-    return _unwrap_return_value(evaluated)
 
 
 def _extend_function_environment(fn: Function, args: List[Object]) -> Environment:
@@ -238,7 +247,9 @@ def _evaluate_identifier(node: ast.Identifier, env: Environment) -> Object:
         return env[node.value]
 
     except KeyError:
-        return _new_error(_UNKNOWN_IDENTIFIER, [node.value])
+
+        return BUILTINS.get(node.value,
+                             _new_error(_UNKNOWN_IDENTIFIER, [node.value]))
 
 
 def _evaluate_bang_operator_expression(right: Object) -> Object:
